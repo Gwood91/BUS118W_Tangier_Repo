fafc745a4d90b1862a91fcc53dd95aa779913f8d
Elliott Mac 2013 <elynch90@gmail.com>
2019-10-13 15:34:49 -0700

    adjusting the route for current user

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 # change the directory to current user
 current_user = user_root = os.path.expanduser('~')
 local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
+sys.path.append(local_path)
 from app import routes
 
