0ad4aec7468000d974109146f6f9649ea2570408
Elliott Mac 2013 <elynch90@gmail.com>
2019-10-21 02:38:33 -0700

    Recruiter Update
    Added WIP new project template and route. Alterations to the base template.

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,27 @@
-# import modules
+# import sys modules
 import sys
-from flask import Flask, g, render_template, redirect, url_for, session, request, flash
 import os
+# define the path for the current user
+current_user = user_root = os.path.expanduser('~')
+local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
+# change the directory to the venv on the machine of the current user
+os.chdir(local_path)
+print(os.getcwd())
+# import modules
+from flask import Flask, g, render_template, redirect, url_for, session, request, flash
 # these two modules are for the candidate checker
 # install this mkl for gensim error
 #from gensim.summarization.summarizer import summarize
 from fuzzywuzzy.fuzz import ratio
-
-# change the directory to current user
-current_user = user_root = os.path.expanduser('~')
-local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
-sys.path.append(local_path)
 from app import app
+
+"""THE FOLLOWING CODE IS DEPRECATED"""
+# change the directory to current user
+#current_user = user_root = os.path.expanduser('~')
+#local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
+# sys.path.append(local_path)
+#from app import app
+"""END DEPRECATION"""
 
 
 # define the routes
@@ -60,6 +70,7 @@ def recruiter_page():
         #current_cand_analysis = "Candidate Match: " + str(candidate_match) + "%\n" + "Summary Match: " + str(summary_match) + "%"
         return render_template('recruiter_page.html', title='Recruiter', candidate_analysis=current_cand_analysis, i=5)
 
+
 @app.route('/candidate_search', methods=['GET', 'POST'])
 def candidate_search():
     if request.method == 'GET':
@@ -69,3 +80,15 @@ def candidate_search():
         # get the data supplied by the client and construct sanitzed queries in the db
         candidate_text = str(request.form.get("candidateText", None))
         search_criteria = str(request.form.get("searchCriteria", None))
+
+
+@app.route('/newProject', methods=['GET', 'POST'])
+def create_project():
+    if request.method == 'GET':
+        return render_template('recruiter_create_project.html', title='Create Project', i=5)
+    # if the recruiter client is evaluating the potential match of a candidate
+    if request.method == 'POST':
+        # get the data supplied by the client and construct sanitzed queries in the db
+        project_title = str(request.form.get("projectTitle", None))
+        project_description = str(request.form.get("projectDescription", None))
+        return str(project_title + project_description + """<a href=" / recruiter" style="float: right"><button>Return to Recruiter View</button></a>""")
