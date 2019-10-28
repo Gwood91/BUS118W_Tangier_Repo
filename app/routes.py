7e43b5c474797323129dc43bb5c2598190fe36c0
Elliott Mac 2013 <elynch90@gmail.com>
2019-10-27 18:03:32 -0700

    Initial Okta Implementation

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,11 @@ from flask import Flask, g, render_template, redirect, url_for, session, request
 #from gensim.summarization.summarizer import summarize
 from fuzzywuzzy.fuzz import ratio
 from app import app
+# modules for okta authentication
+import oidc
+from flask_oidc import OpenIDConnect
+from okta import UsersClient
+from oauth2client.client import OAuth2Credentials
 
 """THE FOLLOWING CODE IS DEPRECATED"""
 # change the directory to current user
@@ -22,13 +27,45 @@ from app import app
 # sys.path.append(local_path)
 #from app import app
 """END DEPRECATION"""
+# preload request
 
+
+# @app.before_request
+# def before_request():
+    # """ Load a proper user object using the user ID from the ID token. This way, the
+     # `g.user` object can be used at any point"""
+    # if oidc.user_loggedin:
+        #g.user = okta_client.get_user(oidc.user_getfield("sub"))
+        #user = db.session.query(User_Profile).filter_by(email=g.user.profile.email).first_or_404()
+        #user_client = user
+    # else:
+        #g.user = None
+
+
+# note: the return variable cannot have the same name as the function that is returning it
+# tangier OKTA Login
+app.config["DEBUG"] = True
+app.config["OIDC_CLIENT_SECRETS"] = "client_secrets.json"
+app.config["OIDC_COOKIE_SECURE"] = False
+app.config["OIDC_CALLBACK_ROUTE"] = "/oidc/callback"
+app.config["OIDC_SCOPES"] = ["openid", "email", "profile"]
+app.config["SECRET_KEY"] = "kadsddhakDabjdshdsadabjkdhjkh2jh3jk395llabife395wlan7dfs9isdf83"
+app.config["OIDC_ID_TOKEN_COOKIE_NAME"] = "Tangier_Token"
+oidc = OpenIDConnect(app)
+# argument includes Elliott's okta dev url and the Tangier Token
+okta_client = UsersClient("https://dev-111947.okta.com", "000zLjpp0BsHcC2LvmtUb5EL1H_rxHqWLNhfJ4ImCs")
 
 # define the routes
 
 
-@app.route('/home')
+@app.route('/home', methods=['GET'])
 def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route('/home', methods=['Post'])
+@oidc.require_login
+def login_handler():
     return render_template('home.html', title='Home')
 
 
