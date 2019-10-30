94a22adfadbfea878fdc4c92bd2e260af1803d8c
Elliott Mac 2013 <elynch90@gmail.com>
2019-10-30 15:44:29 -0700

    DB init
    Ladies and Gentlemen, we have a DB!

--- app/__init__.py
+++ app/__init__.py
@@ -7,21 +7,21 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 
 current_user = os.path.expanduser('~')
-local_engine_path = "sqlite:////" + current_user
+local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
+local_engine_path = "sqlite:////" + local_path + 'app/app.db'
 engine = create_engine(local_engine_path)
 
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = local_engine_path
 
 # Flask-SQLAlchemy initialization
 # db object represents the database
 db = SQLAlchemy(app)
 
-# For migration purposes if needed 
+# For migration purposes if needed
 # change the directory to current user
 local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
 sys.path.append(local_path)
--- app/models.py
+++ app/models.py
@@ -1,17 +1,23 @@
-# User database model/schema 
+# User database model/schema
 import base64
 import os
 from datetime import datetime
-from app import db
 # from sqlalchemy import db
 # define the path for the current user
 current_user = user_root = os.path.expanduser('~')
 local_path = current_user + "/Documents/GitHub/BUS118W_Tangier_Repo/"
 # change the directory to the venv on the machine of the current user
 os.chdir(local_path)
+from __init__ import db, app
+
+
+def init_db():
+    with app.app_context():
+        db.create_all()
 
 
 class User(db.Model):
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -21,8 +27,9 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
-# The Post class are blog posts written by Users 
+# The Post class are blog posts written by Users
 class Post(db.Model):
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     # timestamp Will receive posts in chronological order
@@ -32,3 +39,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+init_db()
